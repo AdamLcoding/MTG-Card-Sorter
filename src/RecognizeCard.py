6624661422a5388844f2a222775176155f69db62
Adam Liston <adamliston2024@tutanota.com>
2023-01-26 03:20:08 -0500

    Ensured a real card will be verified by adding more thorough searches.

--- src/RecognizeCard.py
+++ src/RecognizeCard.py
@@ -1,15 +1,16 @@
-from fuzzywuzzy import fuzz
 from PIL import Image, ImageFilter
+from fuzzywuzzy import fuzz
 import pytesseract
 import requests
 import json
+import re
 import os
 
 # hard coded variables
 cardOnePos = (50, 50)
 cardTwoPos = (50, 50)
 cardWidth = 540
-minSureness = 90
+minSureness = 80
 
 # below this threshold will be considered black
 thresholdBlack = 128
@@ -58,28 +59,60 @@ def thresholdImageWhiteText(img):
 def getTextFrom(img):
     text = pytesseract.image_to_string(img)
     lines = text.split('\n')
-    return lines[0]
+    cleanedText = re.sub(r"[^\w\s,']", '', lines[0])
+    return cleanedText
 
-def verifyCard(rawText):
+def verifyCard(text):
+
+    # check for entire string (fast)
     global minSureness
-
-    # check for entire string
-    searchableText = rawText.replace(" ", "+")
+    searchableText = text.replace(" ", "+")
     response = requests.get(f'https://data.tcgplayer.com/autocomplete?q={searchableText}')
     data = json.loads(response.text)
     for i in range(len(data["products"])):
-        similarity = fuzz.ratio(rawText, data["products"][i]["product-name"])
-        if(similarity >= minSureness):
+        similarity = fuzz.ratio(text, data["products"][i]["product-name"])
+        if(similarity >= minSureness and data["products"][i]["product-line-name"] == "Magic: The Gathering"):
             print("The card was verified as:")
             print(data["products"][i]["product-name"])
-        
+            return True
+    print("The card could not be verified with an exact search")
+
+    # check for each word (medium)
+    textWords = text.split()
+    for i in range(len(textWords)):
+        response = requests.get(f'https://data.tcgplayer.com/autocomplete?q={textWords[i]}')
+        data = json.loads(response.text)
+        for j in range(len(data["products"])):
+            similarity = fuzz.ratio(text, data["products"][j]["product-name"])
+            if(similarity >= minSureness and data["products"][j]["product-line-name"] == "Magic: The Gathering"):
+                print("The card was verified as:")
+                print(data["products"][j]["product-name"])
+                return True
+    print("The card could not be verified by searching it's words")
+
+    # check for each amount of characters before/after each word (slow)
+    startingPoints = []
+    startingPoints.append(0)
+    for i in range(len(textWords)-1):
+        startingPoints.append(len(textWords[i])+1)
+    
+    for i in range(len(startingPoints)):
+        for j in range(startingPoints[i], len(searchableText)+1):
+            currentSearch = searchableText[startingPoints[i]:j]
+            response = requests.get(f'https://data.tcgplayer.com/autocomplete?q={currentSearch}')
+            data = json.loads(response.text)
+            for k in range(len(data["products"])):
+                similarity = fuzz.ratio(text, data["products"][k]["product-name"])
+                if(similarity >= minSureness and data["products"][k]["product-line-name"] == "Magic: The Gathering"):
+                    print("The card was verified as:")
+                    print(data["products"][k]["product-name"])
+                    return True
+    print("The card could not be verified.")
+    return False
+
 
 
 temp = getTextFrom(thresholdImageBlackText(cropCardOne(webcamImage)))
 print(temp)
 
-verifyCard(temp)
-
-#response = requests.get(f'https://data.tcgplayer.com/autocomplete?q={temp}')
-#data = json.loads(response.text)
-#print(data["products"][0]["product-name"])
+temp2 = verifyCard(temp)
